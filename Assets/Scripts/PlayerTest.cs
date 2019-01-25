b73b8ee388ab833efb5a10bc9959ca1a0afb079e
Kuyul <peyopeyo92@gmail.com>
2019-01-25 14:19:35 +1300

    6 movetowards implemented

--- Assets/Scripts/PlayerTest.cs
+++ Assets/Scripts/PlayerTest.cs
@@ -16,22 +16,30 @@ public class PlayerTest : MonoBehaviour
     public float CameraAdjustScalePerDivide = 2.0f;
     public float RotateSpeedPerDivide = 2.0f;
 
-    //Declare State positions
-    public Vector3 Leftspline1Positions;
-    public Vector3 Leftspline2Positions;
-    public Vector3 LeftrightHandIKpositions;
-    public Vector3 LeftleftHandIKpositions;
+    //Declare (Left) State positions
+    public Vector3[] Leftspline1Positions;
+    public Vector3[] Leftspline2Positions;
+    public Vector3[] LeftrightHandIKpositions;
+    public Vector3[] LeftleftHandIKpositions;
+
+    //Declare (Right) State positions
+    public Vector3[] Rightspline1Positions;
+    public Vector3[] Rightspline2Positions;
+    public Vector3[] RightrightHandIKpositions;
+    public Vector3[] RightleftHandIKpositions;
 
     //Declare original positions
     private Vector3 spline1Orig;
     private Vector3 spline2Orig;
     private Vector3 rightHandIKOrig;
     private Vector3 leftHandIKOrig;
+
+    //Declare private variables
     private float DivideLength;
     private float MiddlePixel;
-
     private bool MoveToNextPoint = false;
     private Vector3 NextPoint;
+    private int State = 0; //e.g State: 0 = balanced, -1 = pizza weight 1 to left, 2 = pizza weight 2 to right
 
     //Huehuehue :)
     void Start()
@@ -54,27 +62,53 @@ public class PlayerTest : MonoBehaviour
             //TODO: Calculate the mouse position offset from the center
             var mousePos = Input.mousePosition;
             var numDivides = (mousePos.x - MiddlePixel)/ DivideLength;
-            Debug.Log("mousePosition: " + Input.mousePosition);
 
 
             //The aim for the player is to return the character to its original position
-            var mouseAdjust = numDivides * CameraAdjustScalePerDivide;
+            var mouseAdjust = Mathf.Abs(numDivides * CameraAdjustScalePerDivide);
             var angleAdjust = numDivides * RotateSpeedPerDivide;
-            Debug.Log("angle Adjust: " + spline1Orig);
 
             spline1.localPosition = Vector3.MoveTowards(spline1.localPosition, spline1Orig, mouseAdjust);
+            Debug.Log("spline 1 localposition: " + spline1.localPosition);
+            Debug.Log("spline 1 originalPosition: " + spline1Orig);
             spline2.localPosition = Vector3.MoveTowards(spline2.localPosition, spline2Orig, mouseAdjust);
             rightHandIK.localPosition = Vector3.MoveTowards(rightHandIK.localPosition, rightHandIKOrig, mouseAdjust);
             leftHandIK.localPosition = Vector3.MoveTowards(leftHandIK.localPosition, leftHandIKOrig, mouseAdjust);
             transform.Rotate(Vector3.forward * -angleAdjust * Time.deltaTime);
         }
+        AdjustBalancingPosition();
+    }
 
-        //Make transition to target position for the amount of unbalanced weight
-        spline1.localPosition = Vector3.MoveTowards(spline1.localPosition, Leftspline1Positions, TransitionSpeed);
-        spline2.localPosition = Vector3.MoveTowards(spline2.localPosition, Leftspline2Positions, TransitionSpeed);
-        rightHandIK.localPosition = Vector3.MoveTowards(rightHandIK.localPosition, LeftrightHandIKpositions, TransitionSpeed);
-        leftHandIK.localPosition = Vector3.MoveTowards(leftHandIK.localPosition, LeftleftHandIKpositions, TransitionSpeed);
-        transform.Rotate(Vector3.forward * RotationSpeed * Time.deltaTime);
+    //Depending on the degree of unbalance, adjust the spine and hand positions to a pre-designated localposition
+    private void AdjustBalancingPosition()
+    {
+        //If the unbalance is heavier to the left
+        if (State < 0)
+        {
+            var index = Mathf.Abs(State) - 1;
+            //Make transition to target position for the amount of unbalanced weight
+            spline1.localPosition = Vector3.MoveTowards(spline1.localPosition, Leftspline1Positions[index], TransitionSpeed);
+            spline2.localPosition = Vector3.MoveTowards(spline2.localPosition, Leftspline2Positions[index], TransitionSpeed);
+            rightHandIK.localPosition = Vector3.MoveTowards(rightHandIK.localPosition, LeftrightHandIKpositions[index], TransitionSpeed);
+            leftHandIK.localPosition = Vector3.MoveTowards(leftHandIK.localPosition, LeftleftHandIKpositions[index], TransitionSpeed);
+            transform.Rotate(Vector3.forward * RotationSpeed * Time.deltaTime);
+        }
+        else if (State > 0)
+        {
+            var index = State - 1;
+            //Make transition to target position for the amount of unbalanced weight
+            spline1.localPosition = Vector3.MoveTowards(spline1.localPosition, Rightspline1Positions[index], TransitionSpeed);
+            spline2.localPosition = Vector3.MoveTowards(spline2.localPosition, Rightspline2Positions[index], TransitionSpeed);
+            rightHandIK.localPosition = Vector3.MoveTowards(rightHandIK.localPosition, RightrightHandIKpositions[index], TransitionSpeed);
+            leftHandIK.localPosition = Vector3.MoveTowards(leftHandIK.localPosition, RightleftHandIKpositions[index], TransitionSpeed);
+            transform.Rotate(Vector3.back * RotationSpeed * Time.deltaTime);
+        }
+    }
 
+    //Add -1 to add weight to the left
+    //Add +1 to add weight to the right
+    public void AddState(int value)
+    {
+        State += value;
     }
 }
